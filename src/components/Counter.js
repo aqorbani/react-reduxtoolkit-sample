575f7bc75059d00cd6de66687e410fb6dd60e66d
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-21 16:28:30 +0330

    use dispatch to run increment and decrement function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,21 @@
-import { useSelector } from "react-redux";
-import { selectCounter } from "../features/counter/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  selectCounter,
+} from "../features/counter/counterSlice";
 
 const Counter = () => {
   const counter = useSelector(selectCounter);
-  return <div>{counter}</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+    </div>
+  );
 };
 
 export default Counter;
